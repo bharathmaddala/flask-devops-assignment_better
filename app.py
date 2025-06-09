4c95d69f0926129ae29859a6d1b0d3e69ba0cb4d
bharathmaddala <bharathmaddala2681@gmail.com>
2025-06-09 15:39:12 +0530

    Change in code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask DevOps!"
+    return "Hello from Bharath Maddala!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
