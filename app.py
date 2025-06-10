385873782bbc45a50b045be207f992a56a75a357
Bharath Maddala <157683940+bharathmaddala@users.noreply.github.com>
2025-06-10 14:24:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Heii Sir...This is Bharath Maddala!!!"
+    return "Thank you for the oppurtunity sir... Looking forward for the feedbak!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
